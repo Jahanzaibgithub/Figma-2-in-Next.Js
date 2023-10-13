12601b3f661f45ec61150f4ac5951bb86d25f72f
Jahanzaib <jahanzaibkhan081@gmail.com>
2023-10-13 14:59:52 +0500

    Partners

--- components/Partners/partners.jsx
+++ components/Partners/partners.jsx
@@ -12,75 +12,77 @@ const Partners = () => {
           freelancers
         </p>
       </div>
-<div className="PartnersSocialContainer flex flex-col p-4 items-center gap-8 lg:gap-0 lg:p-0 lg:flex lg:flex-wrap lg:flex-row lg:justify-center xl:w-10/12 2xl:w-8/12">
-        <div className="PartnersSocial1 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white  w-[273px] h-[151px] lg:w-[273px] lg:h-[163px] lg:py-8">
+      <div className="PartnersSocialContainer flex flex-col p-4 items-center gap-8 lg:gap-0 lg:p-0 lg:flex lg:flex-wrap lg:flex-row lg:justify-center lg:items-center lg:max-w-4xl xl:max-w-6xl">
+        <div className="PartnersSocial1 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white w-[273px] h-[151px] lg:w-[213px] lg:h-[131px] xl:w-[273px] xl:h-[163px] lg:py-8">
           <h1 clasName="text-second-text font-[actor] text-base font-normal leading-6 tracking-normal">
             Client Name
           </h1>
           <div className="SocialImage ">
-            <img src="/assets/Google.png" alt="" />
+            <img src="/assets/Google.png" alt="" className="lg:w-48" />
           </div>
         </div>
-        <div className="PartnersSocial2 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white  w-[273px] h-[151px] lg:w-[273px] lg:h-[160px] lg:py-8">
+        <div className="PartnersSocial2 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white w-[273px] h-[151px] lg:w-[203px] lg:h-[128px] xl:w-[273px] xl:h-[160px] lg:py-8">
           <h1 clasName="text-second-text font-[actor] text-base font-normal leading-6 tracking-normal">
             Client Name
           </h1>
           <div className="SocialImage ">
-            <img src="/assets/Amazon.png" alt=""  />
+            <img src="/assets/Amazon.png" alt="" className="lg:w-52" />
           </div>
         </div>
-        <div className="PartnersSocial3 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white w-[273px] h-[151px] lg:w-[300px] lg:h-[165px] lg:py-8">
+        <div className="PartnersSocial3 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white w-[273px] h-[151px] lg:w-[215px] lg:h-[133px] xl:w-[300px] xl:h-[165px] lg:py-8">
           <h1 clasName="text-second-text font-[actor] text-base font-normal leading-6 tracking-normal">
             Client Name
           </h1>
           <div className="SocialImage ">
-            <img src="/assets/Microsoft.png" alt=""  />
-          </div>
-        </div>  
-        <div className="PartnersSocial4 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white w-[273px] h-[151px] lg:w-[220px] lg:h-[165px] lg:py-8">
-          <h1 clasName="text-second-text font-[actor] text-base font-normal leading-6 tracking-normal">
-            Client Name
-          </h1>
-          <div className="SocialImage ">
-            <img src="/assets/Uber.png" alt="" />
+            <img src="/assets/Microsoft.png" alt="" className="lg:w-56" />
           </div>
         </div>
-        <div className="PartnersSocial5 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white w-[273px] h-[151px] lg:w-[309px] lg:h-[164px] lg:py-8">
+        <div className="PartnersSocial4 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white w-[273px] h-[151px] lg:w-[185px] lg:h-[133px] xl:w-[220px] xl:h-[165px] lg:py-8">
           <h1 clasName="text-second-text font-[actor] text-base font-normal leading-6 tracking-normal">
             Client Name
           </h1>
           <div className="SocialImage ">
-            <img src="/assets/Dropbox.png" alt=""/>
+            <img src="/assets/Uber.png" alt="" className="lg:w-36" />
           </div>
         </div>
-        <div className="PartnersSocial6 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white w-[273px] h-[151px] lg:w-[273px] lg:h-[163px] lg:py-8">
+        <div className="PartnersSocial5 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white w-[273px] h-[151px] lg:w-[225px] lg:h-[133px] xl:w-[300px] xl:h-[165px] lg:py-8">
           <h1 clasName="text-second-text font-[actor] text-base font-normal leading-6 tracking-normal">
             Client Name
           </h1>
           <div className="SocialImage ">
-            <img src="/assets/Google.png" alt="" />
+            <img src="/assets/Dropbox.png" alt=""  className="lg:w-56" />
           </div>
         </div>
-        <div className="PartnersSocial7 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white w-[273px] h-[151px] lg:w-[220px] lg:h-[165px] lg:py-8">
+        <div className="PartnersSocial6 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white  w-[273px] h-[151px] lg:w-[205px] lg:h-[131px] xl:w-[273px] xl:h-[163px] lg:py-8">
           <h1 clasName="text-second-text font-[actor] text-base font-normal leading-6 tracking-normal">
             Client Name
           </h1>
-          <div className="SocialImage ">
-            <img src="/assets/Uber.png" alt="" />
+          <div className="SocialImage "> 
+            <img src="/assets/Google.png" alt="" className="lg:w-48" />
           </div>
         </div>
-        <div className="PartnersSocial8 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white w-[273px] h-[151px] lg:w-[273px] lg:h-[160px] lg:py-8">
+        <div className="PartnersSocial7 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white w-[273px] h-[151px] lg:w-[185px] lg:h-[131px] xl:w-[220px] xl:h-[165px] lg:py-8">
           <h1 clasName="text-second-text font-[actor] text-base font-normal leading-6 tracking-normal">
             Client Name
           </h1>
           <div className="SocialImage ">
-            <img src="/assets/Amazon.png" alt="" />
+            <img src="/assets/Uber.png" alt="" className="lg:w-36" />
+          </div>
+        </div>
+        <div className="PartnersSocial8 flex py-6 px-10 flex-col items-center gap-4 border border-solid border-gray-300 bg-var-light-background bg-white w-[273px] h-[151px] lg:w-[205px] lg:h-[126px] xl:w-[273px] xl:h-[158px] lg:py-8">
+          <h1 clasName="text-second-text font-[actor] text-base font-normal leading-6 tracking-normal">
+            Client Name
+          </h1>
+          <div className="SocialImage ">
+            <img src="/assets/Amazon.png" alt="" className="lg:w-36" />
           </div>
         </div>
       </div>
-        <div className="PartnersButton flex w-60 py-4 flex-col items-center rounded-full bg-var-Primary bg-blue-500">
-                    <h1 className="text-white font-[actor] text-xl font-normal leading-7 tracking-tighter">Try For Free</h1>
-        </div>
+      <div className="PartnersButton flex w-60 py-4 flex-col items-center rounded-full bg-var-Primary bg-blue-500">
+        <h1 className="text-white font-[actor] text-xl font-normal leading-7 tracking-tighter">
+          Try For Free
+        </h1>
+      </div>
     </div>
   );
 };
